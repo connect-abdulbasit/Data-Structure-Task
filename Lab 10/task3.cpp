95aaf511dd3c46a50f63cc89a63eac13d7495605
Abdul Basit <contact.abdulbasit.cs@gmail.com>
2024-11-19 20:39:19 +0500

    task3 done

--- Lab 10/task3.cpp
+++ Lab 10/task3.cpp
@@ -1,7 +1,86 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+struct Compare
+{
+    bool operator()(ListNode *a, ListNode *b)
+    {
+        return a->val > b->val;
+    }
+};
+
+ListNode *mergeKLists(vector<ListNode *> &lists)
+{
+    priority_queue<ListNode *, vector<ListNode *>, Compare> minHeap;
+
+    for (ListNode *list : lists)
+    {
+        if (list)
+        {
+            minHeap.push(list);
+        }
+    }
+
+    ListNode *dummy = new ListNode(0);
+    ListNode *current = dummy;
+
+    while (!minHeap.empty())
+    {
+        ListNode *smallest = minHeap.top();
+        minHeap.pop();
+
+        current->next = smallest;
+        current = current->next;
+
+        if (smallest->next)
+        {
+            minHeap.push(smallest->next);
+        }
+    }
+
+    return dummy->next;
+}
+
+ListNode *createList(const vector<int> &values)
+{
+    ListNode *dummy = new ListNode(0);
+    ListNode *current = dummy;
+    for (int value : values)
+    {
+        current->next = new ListNode(value);
+        current = current->next;
+    }
+    return dummy->next;
+}
+
+void printList(ListNode *head)
+{
+    while (head)
+    {
+        cout << head->val << " -> ";
+        head = head->next;
+    }
+    cout << "NULL" << endl;
+}
+
 int main()
 {
+    vector<ListNode *> lists = {
+        createList({1, 4, 5}),
+        createList({1, 3, 4}),
+        createList({2, 6})};
+
+    ListNode *merged = mergeKLists(lists);
+    printList(merged);
 
     return 0;
 }
